8094e8859ce0c466346f68b8d4db80eb068fb22c
Martin Ogg <2martinogg@gmail.com>
2018-02-26 14:29:33 +0000

    Initial Commit

---
+++ fruitViperObjC/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  fruitViperObjC
+//
+//  Created by martin ogg on 26/02/2018.
+//  Copyright © 2018 martinogg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ fruitViperObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fruitViperObjC
+//
+//  Created by martin ogg on 26/02/2018.
+//  Copyright © 2018 martinogg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
